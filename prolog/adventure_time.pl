0b8a502bc70bcbb9a13eb154350978e17c320914
Riley Primeau <primeauriley@gmail.com>
2023-05-03 13:49:15 -0600

    prolog code and screenshots

--- prolog/adventure_time.pl
+++ prolog/adventure_time.pl
@@ -1,5 +1,11 @@
-help :- write('This is sample Prolog DB Defining Adventure Time'), nl,
-write(' This program is written into at.pl file. ').
+help :- write('This is sample Prolog DB Defining Adventure Time. Get to know the characters'), nl,
+write('   COMMANDS '), nl,
+write(' appeared_in(2)'), nl,
+write(' appeared_with(2)'), nl,
+write(' friends(2)'), nl,
+write(' brothers(2)'), nl,
+write(' enemies(2)'), nl,
+write(' wins_against(2)').
 
 % Facts
 character('Finn').
@@ -10,8 +16,8 @@ character('Ice King').
 character('BMO').
 character('Lumpy Space Princess').
 character('Flame Princess').
-character('All characters')
-character('No characters')
+character('All characters').
+character('No characters').
 
 appearance('Finn', 'Pilot').
 appearance('Jake', 'Pilot').
@@ -23,15 +29,15 @@ appearance('Lumpy Space Princess', 'Trouble in Lumpy Space').
 appearance('Flame Princess', 'Hot to the Touch').
 
 relationship('Finn', 'Jake', 'Brothers').
-realtionship('Jake', 'Finn', 'Brothers').
 relationship('Finn', 'Princess Bubblegum', 'Friend').
-relationship('Finn', 'Marceline', 'Friend').
+relationship('Finn', 'Ice King', 'Enemies').
 relationship('Jake', 'Lady Rainicorn', 'Girlfriend').
 relationship('Ice King', 'Gunther', 'Pet').
 
-stronger_than('Finn', 'Ice King')
-stronger_than('Finn', 'BMO')
-stronger_than('Finn', 'Lumpy Space Princess')
+stronger_than('Finn', 'Ice King').
+stronger_than('Marceline', 'Finn').
+stronger_than('BMO', 'All characters').
+
 
 % Rules
 appeared_in(Character, Episode) :- appearance(Character, Episode).
@@ -39,4 +45,5 @@ appeared_with(Character1, Character2) :- appearance(Character1, Episode), appear
 friends(Character1, Character2) :- relationship(Character1, Character2, 'Friend').
 siblings(Character1, Character2) :- relationship(Character1, Character2, 'Brothers').
 enemies(Character1, Character2) :- relationship(Character1, Character2, 'Enemies').
+wins_against(Character1, Character2) :- stronger_than(Character1, Character).
 
