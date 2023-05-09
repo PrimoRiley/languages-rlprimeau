f31265b49051bbcbc1812aa307354561a944952d
Riley Primeau <primeauriley@gmail.com>
2023-05-09 12:18:01 -0600

    javascript

---
+++ stocks/test.js
@@ -0,0 +1,5 @@
+// Get a reference to the container div element
+var container = document.getElementById("container");
+
+// Set the text content of the container element to "Hi"
+container.textContent = "Hi";
